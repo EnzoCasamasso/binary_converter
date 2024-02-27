dfc1e817556ccddd53ad5b5bd5aeb4dec807f84b
Enzo <Enzo>
2024-02-26 23:42:36 -0300

    complete draw

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+use file::{diff_files, read_file};
+use view::FileDiffViewer;
+
 mod file;
 mod view;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Try use {} <file>", args[0]);
+    if args.len() != 3 {
+        eprintln!("Usage: {} <file1> <file2>", args[0]);
         std::process::exit(1);
     }
+
+    let file1_data = read_file(&args[1])?;
+    let file2_data = read_file(&args[2])?;
+    let diffs = diff_files(&file1_data, &file2_data);
+
+    let mut viewer = FileDiffViewer::new(diffs);
+    viewer.run()
 }
